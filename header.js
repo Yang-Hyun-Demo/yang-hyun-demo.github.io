9c75d0c38244525adecc1b5e61fbd8349da63bba
Nguyễn Ngọc Thy <44516537+ThyNguyen111996@users.noreply.github.com>
2022-05-19 13:56:24 +0700

    modify  home page url

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 //const HOME_PAGE_URL  = 'file:///D:/CyberLogitec%20VietNam%20Company/yanghyun/index.html'; //local url
-const HOME_PAGE_URL  = 'https://jethrokingpear.github.io/demoYanghyun/';
+const HOME_PAGE_URL  = 'https://yang-hyun-demo.github.io/';
 function backHomePage(){
   if (!event.ctrlKey) {
     window.open(HOME_PAGE_URL, '_self');
